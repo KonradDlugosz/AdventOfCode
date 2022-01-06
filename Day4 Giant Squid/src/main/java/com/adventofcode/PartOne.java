594153c620c98479cf458a5246e98c8afe134883
KonradDlugosz <56347935+KonradDlugosz@users.noreply.github.com>
2022-01-06 12:30:47 +0000

    added content

--- Day4 Giant Squid/src/main/java/com/adventofcode/PartOne.java
+++ Day4 Giant Squid/src/main/java/com/adventofcode/PartOne.java
@@ -1,4 +1,10 @@
 package com.adventofcode;
 
+import static com.adventofcode.ReadFile.*;
+
 public class PartOne {
+    public static void main(String[] args) {
+        readFile();
+        System.out.println(drawNumbers);
+    }
 }
--- Day4 Giant Squid/src/main/java/com/adventofcode/ReadFile.java
+++ Day4 Giant Squid/src/main/java/com/adventofcode/ReadFile.java
@@ -1,4 +1,34 @@
 package com.adventofcode;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ReadFile {
+    public static String drawNumbers;
+    public static List<String[][]> boardList = new ArrayList<>();
+
+    public static void readFile()  {
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader("input.txt"));
+            //Read drawNumbers
+            String lineOfText = bufferedReader.readLine();
+            drawNumbers = lineOfText;
+
+            //Read boards
+            while((lineOfText = bufferedReader.readLine()) != null){
+                String [] boards = lineOfText.split(" ");
+                System.out.println(Arrays.toString(boards));
+            }
+
+            //Convert
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
